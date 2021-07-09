858475385c0198b99719a41253bc4840cd2536ed
ahmed kucuk <ahmed.kucuk123@gmail.com>
2021-07-09 15:48:17 +0200

    signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+   get '/contact',    to: 'pages#contact'
+   get '/careers',    to: 'pages#careers'
+   get '/stories',    to: 'pages#stories'
+   get '/services',    to: 'pages#services'
 end
