231c43a611bf8a404bcbb1957b990a6c1e868e27
ahmed kucuk <ahmed.kucuk123@gmail.com>
2021-07-14 15:22:04 +0200

    changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :contact, :careers, :contact, :services, :stories ]
+  skip_before_action :authenticate_user!, only: [ :home, :contact, :careers, :contact, :services, :stories, :clients ]
 
   def home
   end
