2439035f8cb57212d9b7e0a5aa2104fe20588f16
ahmed kucuk <ahmed.kucuk123@gmail.com>
2021-09-07 15:16:32 +0200

    ahmo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_03_125708) do
+ActiveRecord::Schema.define(version: 2021_09_07_131236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,17 @@ ActiveRecord::Schema.define(version: 2021_08_03_125708) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
+  end
+
   create_table "stories", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -68,6 +79,8 @@ ActiveRecord::Schema.define(version: 2021_08_03_125708) do
     t.string "fourth_header"
     t.string "fourth_description"
     t.string "link"
+    t.string "slug"
+    t.index ["slug"], name: "index_stories_on_slug", unique: true
   end
 
   create_table "users", force: :cascade do |t|
