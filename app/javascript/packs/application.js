f09385be140b0acb3b163b082ab4e3f731ed9ec7
ahmed kucuk <ahmed.kucuk123@gmail.com>
2021-09-09 14:26:07 +0200

    ahmo

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("custom/main")
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
