d0f38ea22dc0e39b35edb37d62e96a83b4cf73b5
Adnan1921 <68505052+Adnan1921@users.noreply.github.com>
2022-08-15 15:41:27 +0200

    jos malo pa sad ce

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,18 +1,31 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "https://mita.agency" }
-  config.action_mailer.delivery_method = :smtp
-config.action_mailer.default_url_options = { 
-:host => 'https://mita.agency', 
-:protocol => 'https'
-}
+#   config.action_mailer.default_url_options = { host: "https://mita.agency" }
+#   config.action_mailer.delivery_method = :smtp
+# config.action_mailer.default_url_options = { 
+# :host => 'https://mita.agency', 
+# :protocol => 'https'
+# }
+# config.action_mailer.smtp_settings = {
+# :address => 'smtp.gmail.com',
+# :port => 587,
+# :user_name => 'adnan@mita.ba',
+# :password => 'ndivpmbrtsqyeekc',
+# :authentication => 'plain',
+# :enable_starttls_auto => true
+# }
+
+
+config.action_mailer.default_url_options = {host:'https://mita.agency'}
+config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
-:address => 'smtp.gmail.com',
-:port => 587,
-:user_name => 'adnan@mita.ba',
-:password => 'ndivpmbrtsqyeekc',
-:authentication => 'plain',
-:enable_starttls_auto => true
-}
+  address:              'smtp.gmail.com',
+  port:                 587,
+  user_name:            'adnan@mita.ba',
+  password:             'ndivpmbrtsqyeekc',
+  authentication:       'plain',
+  enable_starttls_auto: true,
+  open_timeout:         5,
+  read_timeout:         5 }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
