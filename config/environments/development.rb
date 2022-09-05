36be7af804b5d14a4259eeb62ed1a488ca9c1074
Adnan1921 <68505052+Adnan1921@users.noreply.github.com>
2022-09-05 12:51:54 +0200

    reCaptcha

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,6 +14,7 @@ Rails.application.configure do
 # }
 
 
+
 config.action_mailer.default_url_options = {host:'localhost'}
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
