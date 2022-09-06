b1c0f2219665d3fbcd8393a2d951efc4572c0681
Adnan1921 <68505052+Adnan1921@users.noreply.github.com>
2022-09-06 11:41:09 +0200

    sitemap3

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,25 +1,28 @@
-# Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://www.mita.agency"
+require 'sitemap_generator'
 
+SitemapGenerator::Sitemap.default_host = 'https://mita.agency'
 SitemapGenerator::Sitemap.create do
-  
-  sitemap_opts = {
-    create_index: false,
-    default_host: 'https://mita.agency',
-    compress: false,
-    sitemaps_path: '',
-    namer: SitemapGenerator::SimpleNamer.new(:sitemap)
-  }
-  
-  SitemapGenerator::Sitemap.create sitemap_opts do
-    add '/contact', changefreq: 'weekly'
-    add '/services', changefreq: 'weekly'
-    add '/stories', changefreq: 'weekly'
-    add '/clients', changefreq: 'weekly'
+  add '/', :changefreq => 'daily', :priority => 0.9
+  add '/contacts', :changefreq => 'weekly'
+  add '/about', :changefreq => 'weekly'
+  add '/ecommerce', :changefreq => 'weekly'
+  add '/webdesign', :changefreq => 'weekly'
+  add '/content-creation', :changefreq => 'weekly'
+  add '/graphic-design', :changefreq => 'weekly'
+  add '/ppc-marketing', :changefreq => 'weekly'
+  add '/seo', :changefreq => 'weekly'
+  add '/social-media', :changefreq => 'weekly'
+  add '/consulting', :changefreq => 'weekly'
+  add '/stories', :changefreq => 'weekly'
+  add '/clients', :changefreq => 'weekly'
 
 
-  
-  
-    add_to_index "blog_sitemap.xml", host: ENV['SITE_URL']
-  
+
+
+
+
+
+
+
 end
+
