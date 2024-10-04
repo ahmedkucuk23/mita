7e1b124c45c6ecfa3f6b1837a67c844f54c6b128
ahmed kucuk <ahmed.kucuk123@gmail.com>
2024-10-04 20:50:13 +0200

    cganegs

--- app/models/storie.rb
+++ app/models/storie.rb
@@ -1,4 +1,38 @@
 class Storie < ApplicationRecord
+
+  def self.ransackable_attributes(auth_object = nil)
+    [
+      "created_at",
+      "description",
+      "fifth_number",
+      "fifth_number_text",
+      "first_description",
+      "first_header",
+      "first_number",
+      "first_number_text",
+      "fourth_description",
+      "fourth_header",
+      "fourth_number",
+      "fourth_number_text",
+      "id",
+      "link",
+      "name",
+      "second_description",
+      "second_header",
+      "second_number",
+      "second_number_text",
+      "six_number",
+      "six_number_text",
+      "slug",
+      "third_description",
+      "third_header",
+      "third_number",
+      "third_number_text",
+      "updated_at",
+      "user_id"
+    ]
+  end
+
   extend FriendlyId
   friendly_id :name, use: :slugged
   belongs_to :user
